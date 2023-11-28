8c4c4c8d35b20e98a95db371bcab5d9c55b89a57
ToolGeen <htemohpab@gmail.com>
2023-11-28 10:27:45 +0600

    lesson1 tariffs

--- bin/lesson1.dart
+++ bin/lesson1.dart
@@ -1,61 +1,46 @@
 import 'package:args/args.dart';
 
-const String version = '0.0.1';
+void main() {
+  var tariffEco = 250.0;
+  var tariffStandard = 200.0;
+  var tariffPrem = 150.0;
+  var tariffBoundEcoToStandard = 10;
+  var tariffBoundStardardToPrem = 20;
 
-ArgParser buildParser() {
-  return ArgParser()
-    ..addFlag(
-      'help',
-      abbr: 'h',
-      negatable: false,
-      help: 'Print this usage information.',
-    )
-    ..addFlag(
-      'verbose',
-      abbr: 'v',
-      negatable: false,
-      help: 'Show additional command output.',
-    )
-    ..addFlag(
-      'version',
-      negatable: false,
-      help: 'Print the tool version.',
-    );
-}
+  double totalDistance = 25.0;
+  double currentDistance = 0.0;
 
-void printUsage(ArgParser argParser) {
-  print('Usage: dart lesson1.dart <flags> [arguments]');
-  print(argParser.usage);
-}
+  double totalFare = 0.0;
 
-void main(List<String> arguments) {
-  final ArgParser argParser = buildParser();
-  try {
-    final ArgResults results = argParser.parse(arguments);
-    bool verbose = false;
+  while (currentDistance < totalDistance) {
+    double remainingDistance = totalDistance - currentDistance;
 
-    // Process the parsed arguments.
-    if (results.wasParsed('help')) {
-      printUsage(argParser);
-      return;
-    }
-    if (results.wasParsed('version')) {
-      print('lesson1 version: $version');
-      return;
-    }
-    if (results.wasParsed('verbose')) {
-      verbose = true;
+    double currentTariff;
+    if (currentDistance < tariffBoundEcoToStandard) {
+      currentTariff = tariffEco;
+    } else if (currentDistance < tariffBoundStardardToPrem) {
+      currentTariff = tariffStandard;
+    } else {
+      currentTariff = tariffPrem;
     }
 
-    // Act on the arguments provided.
-    print('Positional arguments: ${results.rest}');
-    if (verbose) {
-      print('[VERBOSE] All arguments: ${results.arguments}');
-    }
-  } on FormatException catch (e) {
-    // Print usage information if an invalid argument was provided.
-    print(e.message);
-    print('');
-    printUsage(argParser);
+    double fare = calculateFare(remainingDistance, currentTariff);
+
+    totalFare += fare;
+
+    currentDistance = ((currentDistance < tariffBoundEcoToStandard)
+        ? tariffBoundEcoToStandard
+        : (currentDistance < tariffBoundStardardToPrem)
+        ? tariffBoundStardardToPrem
+        : totalDistance).toDouble();
+
+    print("Расстояние: $currentDistance км, Тариф: $currentTariff тенге, Стоимость: $fare тенге");
   }
+
+  print("Итоговая стоимость: $totalFare тенге");
+  print("Пройденное расстояние: $totalDistance км");
+}
+
+double calculateFare(double distance, double tariff) {
+  return distance * tariff;
 }
